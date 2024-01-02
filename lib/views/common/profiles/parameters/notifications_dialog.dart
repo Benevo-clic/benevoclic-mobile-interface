278e15042fa4151a5c83200a2e91895e59849d93
Geoffrey Herman <geoffrey.herman.etu@univ-lille.fr>
2024-01-03 00:23:56 +0100

    ajout de toutes les notifications sur l'alert dialog associé

--- lib/views/common/profiles/parameters/notifications_dialog.dart
+++ lib/views/common/profiles/parameters/notifications_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:namer_app/widgets/switch_button.dart';
 import 'package:namer_app/widgets/title_with_icon.dart';
+import 'package:namer_app/widgets/title_with_switch.dart';
 
 class NotificationsDialog extends StatefulWidget {
   @override
@@ -13,6 +14,11 @@ class _NotificationsState extends State<NotificationsDialog> {
   final _formKey = GlobalKey<FormState>();
 
   bool messages = false;
+  bool newAnnouncment = false;
+  bool expirationAnnouncement = false;
+  bool completeMission = false;
+  bool newAdhesion = false;
+  bool volunteerAccepted = false;
 
   @override
   Widget build(BuildContext context) {
@@ -22,8 +28,39 @@ class _NotificationsState extends State<NotificationsDialog> {
       });
     }
 
+    changeNewAnnouncement(bool value) {
+      setState(() {
+        newAnnouncment = value;
+      });
+    }
+
+    changeAnnouncement(bool value) {
+      setState(() {
+        expirationAnnouncement = value;
+      });
+    }
+
+    changeCompleteMission(bool value) {
+      setState(() {
+        completeMission = value;
+      });
+    }
+
+    changeNewAdhesion(bool value) {
+      setState(() {
+        newAdhesion = value;
+      });
+    }
+
+    changeVolunteerAccepted(bool value) {
+      setState(() {
+        volunteerAccepted = value;
+      });
+    }
+
     return Column(children: [
-      TitleWithIcon(title: "password", icon: Icon(Icons.admin_panel_settings)),
+      TitleWithIcon(
+          title: "Notifications", icon: Icon(Icons.notification_important)),
       Form(
           key: _formKey,
           child: Column(
@@ -31,35 +68,44 @@ class _NotificationsState extends State<NotificationsDialog> {
               SizedBox(
                 height: 25,
               ),
-              SwitchButton(
-                value: messages,
-                fct: changeNewMessages,
-              ),
-              TitleWithSwitch(content: SwitchButton(
-                value: messages,
-                fct: changeNewMessages,
-              ), text: "Mise en ligne des annonces"),
-              SizedBox(
-                height: 10,
-              ),
-              SizedBox(
-                height: 10,
-              ),
+              TitleWithSwitch(
+                  content: SwitchButton(
+                    value: messages,
+                    fct: changeNewMessages,
+                  ),
+                  text: "Nouveaux messages"),
+              TitleWithSwitch(
+                  content: SwitchButton(
+                    value: newAnnouncment,
+                    fct: changeNewAnnouncement,
+                  ),
+                  text: "Mise en ligne des annonces"),
+              TitleWithSwitch(
+                  content: SwitchButton(
+                    value: expirationAnnouncement,
+                    fct: changeAnnouncement,
+                  ),
+                  text: "Expiration des annonces"),
+              TitleWithSwitch(
+                  content: SwitchButton(
+                    value: completeMission,
+                    fct: changeCompleteMission,
+                  ),
+                  text: "Annoncer lorsqu'une mission est complète"),
+              TitleWithSwitch(
+                  content: SwitchButton(
+                    value: newAdhesion,
+                    fct: changeNewAdhesion,
+                  ),
+                  text: "Annoncer les demandes d'adhésion"),
+              TitleWithSwitch(
+                  content: SwitchButton(
+                    value: volunteerAccepted,
+                    fct: changeVolunteerAccepted,
+                  ),
+                  text: "Annoncer lorqu'un benevole accepte une mission"),
             ],
           ))
     ]);
-  }
-}
-
-class TitleWithSwitch extends StatelessWidget {
-  String text;
-  dynamic content;
-
-  TitleWithSwitch({required this.text, required this.content});
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [Expanded(child: Text(text)), content],
-    );
   }
 }

bff5df3c8a7aeea490f3b305bb826aa61877e431
Geoffrey Herman <geoffrey.herman.etu@univ-lille.fr>
2024-01-02 22:07:31 +0100

    création dialog pour notifications

---
+++ lib/views/common/profiles/parameters/notifications_dialog.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:namer_app/views/common/profiles/widget/pop_dialog.dart';
+
+class Notifications extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return PopDialog(
+      content: Text(""),
+    );
+  }
+
+}

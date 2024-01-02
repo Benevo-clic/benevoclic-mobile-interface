72cdb4e75985a13e353423bc3ffe172d8c1757e7
Geoffrey Herman <geoffrey.herman.etu@univ-lille.fr>
2024-01-02 23:35:08 +0100

    création du bouton switch

---
+++ lib/widgets/switch_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SwitchButton extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _SwitchButtonState();
+  }
+
+}
+
+class _SwitchButtonState extends State<SwitchButton>{
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+  
+
+}

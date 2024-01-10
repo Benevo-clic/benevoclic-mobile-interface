5a69f04c886e5059dab6f56de875dfdf168a567d
Geoffrey Herman <geoffrey.herman.etu@univ-lille.fr>
2024-01-10 13:01:52 +0100

    la réinitialisation du mot de passe est fonctionnel (envoie d'un mail à l'adresse pour changer mot de passe)

--- lib/views/common/authentification/forgotten_password.dart
+++ lib/views/common/authentification/forgotten_password.dart
@@ -1,15 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:namer_app/repositories/google/auth_repository.dart';
 import 'package:namer_app/type/rules_type.dart';
+import 'package:namer_app/util/color.dart';
+import 'package:namer_app/util/email_verification.dart';
+import 'package:namer_app/widgets/button.dart';
 
 class ForgottenPassword extends StatelessWidget {
   final RulesType roleType;
 
-  const ForgottenPassword({super.key, required this.roleType});
+  ForgottenPassword({super.key, required this.roleType});
 
+  final _controller = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [Text("oui ")],
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          Text("Entrez votre adresse mail ci-dessous"),
+          TextFormField(
+            validator: (value) {
+              EmailVerification ev = EmailVerification(value!);
+              if (ev.security()) {
+                return null;
+              } else {
+                return ev.message;
+              }
+            },
+            controller: _controller,
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Button(
+              text: "Réinitiliser le mot de passe",
+              color: Colors.white,
+              fct: () {
+                if (_formKey.currentState!.validate()) {
+                  AuthRepository().forgotPassword(_controller.text);
+                }
+              },
+              backgroundColor: marron)
+        ],
+      ),
     );
   }
 }
